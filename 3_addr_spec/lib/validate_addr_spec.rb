bbf9e4593d74ae8f4e19782bc0507a730162db3d
masatora <atarashi.masatora@gmail.com>
2020-10-24 18:40:13 +0900

    Implement 3(WIP)

--- 3_addr_spec/lib/validate_addr_spec.rb
+++ 3_addr_spec/lib/validate_addr_spec.rb
@@ -1,7 +1,7 @@
 class ValidateAddrSpec
   def validate_addr_spec(stdin, stdout)
     stdin.readlines.each do |line|
-      if valid?(line)
+      if valid?(line.chomp)
         stdout.puts("ok")
       else
         stdout.puts("ng")
@@ -12,21 +12,32 @@ class ValidateAddrSpec
   def valid?(email)
     # @ is only once
     splited = email.split("@")
-    return false if split.length != 2
+    return false if splited.length != 2
 
     local, domain = splited
     # Domain part assertion
-    # D1
-    return false if domain.match?(/[\w\d!\#$%&'\*\+-\/=?^_`{|}~\.]+/)
+    # D1 && D5
+    return false unless /[\w\d!\#$%&'\*\+-\/=?^_`{|}~\.]+/ === domain
     # D2
-    return false if domain[0]
+    return false if domain[0] == "."
     # D3
-    return false if domain[-1]
+    return false if domain[-1] == "."
     # D4
-    # D5
+    return false if /\.\./ === domain
 
     # Local part assertion
     # L1
-
+    dotatom?(local) || quotedstr?(local)
   end
+
+  def dotatom?(local)
+    return false unless /[\w\d!\#$%&'\*\+-\/=?^_`{|}~\.]+/ === local
+    return false if local[0] == "."
+    true
+  end
+
+  def quotedstr?(local)
+    false
+  end
+
 end
--- 3_addr_spec/test/validate_addr_spec_test.rb
+++ 3_addr_spec/test/validate_addr_spec_test.rb
@@ -9,15 +9,216 @@ class ValidateAddrSpecTest < Minitest::Test
     @vas = ValidateAddrSpec.new
   end
 
-  def test_validate
+  # ドメイン部
+  # 1
+  def test_validate_domain_1
     stdin = StringIO.new  <<EOF
 abc@example.com
-a..bc@example.com
+abc@.あ
 EOF
     stdout = StringIO.new("", 'w+')
     expects = "ok\nng\n"
     @vas.validate_addr_spec(stdin, stdout)
     assert_equal expects, stdout.string
   end
+
+  # 2
+  def test_validate_domain_2
+    stdin = StringIO.new  <<EOF
+abc@example.com
+abc@.example.com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+  # 3
+  def test_validate_domain_3
+    stdin = StringIO.new  <<EOF
+abc@example.com
+abc@example.com.
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+  # 4
+  def test_validate_domain_4
+    stdin = StringIO.new  <<EOF
+abc@example.com
+abc@example..com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+  # 5
+  def test_validate_domain_5
+    stdin = StringIO.new  <<EOF
+abc@example.com
+a..bc@
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+  # @
+  def test_validate_a_mark_1
+    stdin = StringIO.new  <<EOF
+abc@example.com
+abcexample.com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+  def test_validate_a_mark_2
+    stdin = StringIO.new  <<EOF
+abc@example.com
+abc@@example.com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+
+#   # ローカル部
+#   def test_validate_local
+#     # TODO: ドットアトム部またはクオーテッド文字列部のどちらかである
+#     stdin = StringIO.new  <<EOF
+#
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+
+#   # ドットアトム
+#   # 1
+  def test_validate_local_dot_atom_1
+    stdin = StringIO.new  <<EOF
+abc@example.com
+ドットアトム@example.com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+#   # 2
+  def test_validate_local_dot_atom_2
+    stdin = StringIO.new  <<EOF
+abc@example.com
+.abc@example.com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+#   # 3
+  def test_validate_local_dot_atom_3
+    stdin = StringIO.new  <<EOF
+abc@example.com
+abc.@example.com
+EOF
+    stdout = StringIO.new("", 'w+')
+    expects = "ok\nng\n"
+    @vas.validate_addr_spec(stdin, stdout)
+    assert_equal expects, stdout.string
+  end
+
+#   # 4
+#   def test_validate_local_dot_atom_4
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+#   # 5
+#   def test_validate_local_dot_atom_5
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+#   # クオーテッド
+#   # 1
+#   def test_validate_local_quated_1
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+#   # 2
+#   def test_validate_local_quated_2
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+#   # 3
+#   def test_validate_local_quated_3
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+#   # 4
+#   def test_validate_local_quated_4
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
+#   # 5
+#   def test_validate_local_quated_5
+#     stdin = StringIO.new  <<EOF
+# abc@example.com
+# a..bc@example.com
+# EOF
+#     stdout = StringIO.new("", 'w+')
+#     expects = "ok\nng\n"
+#     @vas.validate_addr_spec(stdin, stdout)
+#     assert_equal expects, stdout.string
+#   end
 end
 
